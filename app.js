55414eaf33ebe54a3c1759e6f7dc17d4eef58700
Marcelo Mendes <celinhofm@gmail.com>
2020-07-16 00:16:44 -0300

    get

--- app.js
+++ app.js
@@ -15,23 +15,20 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded());
 
-// app.post('/url', function (req, res) {
-//     const jogoAux = { jogador1: req.body.jogador1, jogador2: req.body.jogador2, gols1: req.body.gols1, gols2: req.body.gols2 };
-//     // const jogoCriado = db.stats_jogos.create({ jogador1: req.body.jogador1, jogador2: req.body.jogador2, gols1: req.body.gols1, gols2: req.body.gols2 }).then(() => {
-//     //     res.send(jogo);
-//     // });
-//     console.log('JOGAO : ', jogoAux);
-//     // res.send(jogoCriado);
-//     db.stats_jogos.create(jogoAux)
-//         .then((jogo) => { res.json(jogo) });
-// });
+app.get('/get', function (req, res) {
+    db.stats_jogos.findAll({ order: [['createdAt', 'DESC']], limit: 15 })
+        .then(jogosObj => {
+            res.send(jogosObj);
+            // res.render('index', {
+            //     jogosObj: jogosObj
+            // });
+        });
+});
 
 app.post('/url', function (req, res) {
-    console.log(req.body);
     const jogoAux = { jogador1: req.body.jogador1, jogador2: req.body.jogador2, gols1: req.body.gols1, gols2: req.body.gols2 };
-    console.log(jogoAux);
-    const jogo = db.stats_jogos.create(req.body).then( () => {
-        res.send(jogo);
+    const jogo = db.stats_jogos.create(req.body).then(() => {
+        res.send(jogoAux);
     });
     console.log(jogo.jogador1);
 });
